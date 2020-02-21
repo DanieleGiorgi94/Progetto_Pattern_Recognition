038598fbc26e05a13aa41842fe7591b214714204
DanieleGiorgi94 <giorgi.daniele@icloud.com>
2020-02-21 11:54:39 +0100

    Divisione dataset in train e test

--- script.m
+++ script.m
@@ -4,7 +4,7 @@ clc
 
 data = readtable('SolarPrediction.csv');
 
-%conversione fahrenheit a celsius
+%conversione fahrenheit in celsius
 data{:,5} = (data{:,5} - 32) * 5/9;
 
 %eliminazione colonna UNIXTime
@@ -16,3 +16,11 @@ data{:,5} = data{:,5} / 29.921;
 
 %conversione miglia in km
 data{:,8} = data{:,8} * 1.60934;
+
+%divisione del dataset in train (90%) e test (10%)
+row_train = 29417;
+% fprintf('%.0f\n',height(data)*90/100) = 29417
+train = data(1:row_train, :);
+test = data(row_train+1:height(data),:);
+
+
