5ec400ef4d1f2b836baebe86bcc5ade00aa24af8
gsmfnc <francesco.gismondi.f@gmail.com>
2020-02-26 01:24:04 +0100

    aggiunti grafici PLS in grafici_dataset_completo

--- full_dataset_pls.m
+++ full_dataset_pls.m
@@ -1,4 +1,3 @@
-
 % inizializzazione
 clear X
 clear y
@@ -10,25 +9,29 @@ clear ytestFitted
 clear ytest
 clear yfit
 
+resid = 1;
+t2 = 0;
 
-X1 = int_pulse(1:2:end,1:10); % xdata in training
-y = conc(1:2:end,:); % ydata in trainng
+%xdata in training
+X1 = training_set;
+%ydata in training
+y = training_y;
 
-Xtest1=int_pulse(2:2:end,1:10); % xdata in test
-ytest=conc(2:2:end,:); % ydata in test
+%xdata in test
+Xtest1 = test_set;
+%ydata in test
+ytest = test_y;
 
-% data normalization
-% norm =1 autoscaling
-% norm=2 mean centering
-% norm=0 no scaling
-%
-norm=1;
+%data normalization
+%norm =1 autoscaling
+%norm=2 mean centering
+%norm=0 no scaling
+norm = 2;
 if norm==1
     [X,MU,SIGMA] = zscore(X1);
      for i=1:length(Xtest1(:,1))
       Xtest(i,:)=(Xtest1(i,:)-MU)./SIGMA;
      end
-    
 elseif norm==2 
      [AA,MU,SIGMA] = zscore(X1);
      for i=1:length(X1(:,1))
@@ -38,53 +41,51 @@ elseif norm==2
      for i=1:length(Xtest1(:,1))
       Xtest(i,:)=(Xtest1(i,:)-MU);
      end
+elseif norm == 0
+    Xtest = Xtest1;
+    X = X1;
 end
- %
- 
- %%
- % Analisi esplorativa dell'informazione delle singole features
- ff=3; % feature che vogliamo correlare con l'uscita
- % N = grado del polinomio con cui vogliamo determinare una possibile 
- %relazione tra la feature e l'uscita
-  N=10;
-  figure;
 
- plot(X(:,ff),y,'ro');
- % per plottare la info sul numero del campione nel grafico
- %plot_all([X(:,ff) y],[1 2 0 0 1]);
+%%
+% Analisi esplorativa dell'informazione delle singole features
+ff=6; % feature che vogliamo correlare con l'uscita
+% N = grado del polinomio con cui vogliamo determinare una possibile 
+%relazione tra la feature e l'uscita
+N=10;
+figure;
+
+plot(X(:,ff),y,'ro');
+% per plottare la info sul numero del campione nel grafico
+%plot_all([X(:,ff) y],[1 2 0 0 1]);
 xlabel(['Feature n: ' num2str(ff)]);
 ylabel('Ouput');
 
 %% 
- 
- 
- 
- % PLS
- 
-kk=10; % numero partizioni
-n_LV=min([min(size(X)),40]); % numero max di LV
+% PLS
 
- % calcolo PLS
- % per cambiare il tipo di validazione guardare l'help della funzione
- % plsregress
-  [XL,YL,XS,YS,BETA,PCTVAR,MSE0,STATS] = plsregress(X,y,n_LV); % MSE0 e' per il calcolo dell'errore in training
+% numero partizioni
+kk=10;
+% numero max di LV
+n_LV=min([min(size(X)),40]);
 
- [XL,YL,XS,YS,BETA,PCTVAR,MSE,STATS] = plsregress(X,y,n_LV,'CV',kk, 'MCReps',10); % per
- %
- %
- %Plot the percent of variance explained in the response variable as a function of the number of components.
+% calcolo PLS
+% per cambiare il tipo di validazione guardare l'help della funzione plsregress
+[XL,YL,XS,YS,BETA,PCTVAR,MSE0,STATS] = plsregress(X,y,n_LV);
+%MSE0 e' per il calcolo dell'errore in training
+[XL,YL,XS,YS,BETA,PCTVAR,MSE,STATS] = plsregress(X,y,n_LV,'CV',kk, 'MCReps',10);
 
+%Plot the percent of variance explained in the response variable as a function
+%of the number of components.
 figure;
 plot(1:n_LV,cumsum(100*PCTVAR(2,:)),'-bo');
 hold on;
 plot(1:n_LV,cumsum(100*PCTVAR(1,:)),'-kd');
-
 xlabel('Number of LV components');
 ylabel('Explained Variance(%)');
 legend(' Y','X');
 title('Cumulative explained variance (%)');
 
-% errore in cross validation
+%errore in cross validation
 figure;
 plot(0:n_LV,MSE0(2,:),'-rd');
 hold on;
@@ -94,39 +95,34 @@ ylabel('Mean squared Error');
 legend(' Training','Cross Validation');
 title(['Mean Square Error (MSE)' ]);
 
-
-% plot dei residui
 yfit = [ones(size(X,1),1) X]*BETA;
 residuals = y - yfit;
 ytestFitted = [ones(size(Xtest,1),1) Xtest]*BETA;
 residuals_test=ytestFitted-ytest;
 
-
-
-
-figure;
-stem(residuals);
-hold on;
-stem(length(y)+1:length(y)+length(ytest),residuals_test)
-
-hold on;
-xlabel('Observation');
-ylabel('Residual');
-legend('Training','Test');
-title( 'Residual plot');
-
-
-
-
-% T2 vs Q
-figure;
-% plot T2 vs Q
-%plot(residuals.^2,STATS.T2,'bo');
-plot_all([residuals.^2 STATS.T2],[1 2 0 0 1]);
-xlabel('Q');
-ylabel('T^2');
-title('T2 vs Q plot');
-
+if resid == 1
+    %plot dei residui
+    figure;
+    stem(residuals);
+    hold on;
+    stem(length(y)+1:length(y)+length(ytest),residuals_test)
+    
+    hold on;
+    xlabel('Observation');
+    ylabel('Residual');
+    legend('Training','Test');
+    title( 'Residual plot');
+end
+if t2 == 1
+    % T2 vs Q
+    figure;
+    % plot T2 vs Q
+    %plot(residuals.^2,STATS.T2,'bo');
+    plot_all([residuals.^2 STATS.T2],[1 2 0 0 1]);
+    xlabel('Q');
+    ylabel('T^2');
+    title('T2 vs Q plot');
+end
 %%
 
 %  predetto vs misurato
@@ -153,7 +149,4 @@ xlabel('Predicted');
 ylabel('Measured');
 title('predicted vs measured');
 text(aa,bb,strcat({'RMSEC='},{num2str(rms_train)}));
-
-%%
-
-text(aa,bb*0.975,strcat({'RMSECV='},{num2str(rms_test)}));
+text(aa,bb*0.9,strcat({'RMSECV='},{num2str(rms_test)}));
--- full_dataset_script.m
+++ full_dataset_script.m
@@ -26,3 +26,13 @@ ydata = data{:,4};
 max_radiation_value = floor(max(ydata)) + 1;
 n_levels = 4;
 sampled_ydata = ceil(ydata / (max_radiation_value / n_levels));
+
+%percentuale del training set
+perc = 70;
+value = height(data) * perc / 100;
+
+training_set = xdata(1:floor(value),:);
+training_y = ydata(1:floor(value));
+
+test_set = xdata(floor(value)+1:length(xdata),:);
+test_y = ydata(floor(value)+1:length(ydata),:);
